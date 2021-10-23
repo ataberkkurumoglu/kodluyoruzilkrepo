eabd71fa4e0ee3cf3a8c4fc40cd89daadff11d0d
ataberkkurumoglu <77547328+ataberkkurumoglu@users.noreply.github.com>
2021-10-23 23:49:12 +0300

    Create sql-ödev5.sql

---
+++ sql-ödev5.sql
@@ -0,0 +1,5 @@
+select title from film where title like '%n' order by length(title) desc limit 5
+
+select title from film where title like '%n' order by length(title) asc limit 5
+
+select last_name from customer where store_id=1 order by last_name limit 4
